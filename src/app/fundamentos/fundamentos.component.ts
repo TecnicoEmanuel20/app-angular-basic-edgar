b1728123f3c32bcba5a36883091e91225ef93c25
TecnicoEmanuel20 <perezanchayaedgaremanuel@gmail.com>
2024-06-01 14:14:43 -0500

    v1

---
+++ src/app/fundamentos/fundamentos.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fundamentos',
+  standalone: true,
+  imports: [],
+  templateUrl: './fundamentos.component.html',
+  styleUrl: './fundamentos.component.css'
+})
+export class FundamentosComponent {
+
+  titulo  = "Desarrollo de aplicaciones Web 1"
+  contador  = 0
+  imagen = "https://dummyimage.com/300"
+
+  constructor(){
+     setInterval(()=> {
+      this.contador++
+
+     },2000)
+  }
+  resetearContador(): void {
+    this.contador = 0;
+  }
+}
